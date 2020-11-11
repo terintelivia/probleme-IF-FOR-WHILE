b9fd7a0b48b37cc556e221ad4d64bad39eedc307
terintelivia <70704787+terintelivia@users.noreply.github.com>
2020-11-11 12:15:51 +0300

    Add files via upload

---
+++ problema 1 IF FOR WHILE.py
@@ -0,0 +1,11 @@
+n=int(input("Introdu numărul: "))
+if ((n<28)or(n>31)):
+    print("nu sunt luni cu asa numar de zile")
+elif n==28:
+    print("februarie, daca nu e an bisect")
+elif n==29:
+    print("februarie, daca e an bisect")
+elif n==30:
+    print("aprilie,iunie,septembrie,noiembrie")
+else:
+    print("ianuarie,martie,mai,iulie,august,octombrie,decembrie")
