1655adb6a63a4c840b42cf4926a785c90aab2545
terintelivia <70704787+terintelivia@users.noreply.github.com>
2020-11-12 10:07:17 +0300

    Update problema 1 IF FOR WHILE.py

--- problema 1 IF FOR WHILE.py
+++ problema 1 IF FOR WHILE.py
@@ -7,5 +7,9 @@ elif n==29:
     print("februarie, daca e an bisect")
 elif n==30:
     print("aprilie,iunie,septembrie,noiembrie")
-else:
-    print("ianuarie,martie,mai,iulie,august,octombrie,decembrie")
+elif n==31:
+     print("ianuarie,martie,mai,iulie,august,octombrie,decembrie")
+   elif (n<28):
+  print("nu sunt luni cu asa numar de zile")
+    elif (n>31):
+ print("nu sunt luni cu asa numar de zile")
