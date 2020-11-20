216528d817314505e308603d9cc5ff6ec108441e
terintelivia <70704787+terintelivia@users.noreply.github.com>
2020-11-20 19:55:35 +0300

    Update problema 9 IF FOR WHILE.py

--- problema 9 IF FOR WHILE.py
+++ problema 9 IF FOR WHILE.py
@@ -1,6 +1,3 @@
-"
-Un număr natural se numește număr perfect dacă este egal cu suma divizorilor lui, în afară de el însuși. De exemplu, 6 este număr perfect ,
-deoarece 6=1+2+3. Să se afle numerele perfecte mai mici decât numărul natural dat n."
 n=int(input("Dati un numar:"))
 suma=0
 for i in range(1,n):
